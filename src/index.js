fa0eadbe649a16ba5c45a11ed515983df2298e36
Sunil kumar <sunilkumar3020@gmail.com>
2024-07-23 15:51:58 +0530

    file changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='https://sunilkumar3020.github.io/react-router'>
+    <BrowserRouter basename='/react-router'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
