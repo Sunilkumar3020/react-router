19938f3e80c955483f71ed06c87b09e94ddcc2b2
Sunil kumar <sunilkumar3020@gmail.com>
2024-07-23 13:03:44 +0530

    New pages create link home, about, project, contact

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="/project">Project</Link></li>
+                <li><Link to="/contact">Contact Us</Link></li>
+            </ul>
+        </nav>
+
+    )
+}
+export default Navbar
---
+++ src/Project.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+const Project = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Project Page</h1>
+        </>
+    )
+}
+export default Project
